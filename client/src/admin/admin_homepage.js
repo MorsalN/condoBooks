c068d72635d552a70620f69536ce50a20036f922
Morsal Niyaz <morsalniyaz@gmail.com>
2022-01-18 22:42:08 -0800

    Created Admin Homepage

--- client/src/admin/admin_homepage.js
+++ client/src/admin/admin_homepage.js
@@ -1,14 +1,61 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, useNavigate, useParams } from "react-router-dom";
+import '../css/Admin.css'
 
-// class Amenities extends Component {
 
 export default function Amenities() {
-  // render() {
-    return (
-      <h1>This is a CURRENT AMENITIES for Admin!</h1>
-    )
-  // }
-}
+  const navigate = useNavigate();
 
-// export default Amenities;
+  const params = useParams()
+  console.log(params)
+
+  function handleClickManage() {
+    navigate(`/${params.user_id}/manage`);
+
+  }
+
+  function handleClickAdd() {
+    navigate(`/${params.user_id}/add`);
+  }
+
+  return (
+
+    <section className="Admin">
+      <div className="Admin-box">
+        <h1 className="Admin-title">Current Amenities</h1> 
+
+          <table className="table">
+            <tbody id="customers">
+              <tr>
+                <th>Room</th>
+                <th>Start</th>
+                <th>End</th>
+                <th>Days</th>
+                <th></th>
+              </tr>
+              <tr>
+                <td>Gym</td>
+                <td>09:00</td>
+                <td>22:00</td>
+                <td>All Days</td>
+                <td><button className="manage" onClick={handleClickManage}>Manage Amenity</button></td>
+              </tr>
+              <tr>
+                <td>Social</td>
+                <td>09:00</td>
+                <td>22:00</td>
+                <td>All Days</td>
+                <td><button className="manage" onClick={handleClickManage}>Manage Amenity</button></td>
+              </tr>
+            </tbody>
+          </table>
+
+          <button className="add" onClick={handleClickAdd}>Add Amenity</button>
+
+
+      </div>
+    </section>
+
+  )
+
+}
