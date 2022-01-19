a2eb1970268294fc2b311f0ba3ea05bebed59e2c
kunvar13 <kunvar13@gmail.com>
2022-01-18 20:51:44 -0600

    Edited migration files

--- db/migrate/20220115165150_create_users.rb
+++ db/migrate/20220115165150_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       
       t.column "first_name", :string, :limit => 50
       t.column "last_name", :string, :limit => 50
+      t.column "email", :string, :limit => 50
       t.column "phone", :string, :default => '', :null => false
       t.column "unit", :string
       t.column "admin", :boolean
--- db/migrate/20220115184248_create_bookings.rb
+++ db/migrate/20220115184248_create_bookings.rb
@@ -3,9 +3,10 @@ class CreateBookings < ActiveRecord::Migration[5.2]
     create_table :bookings do |t|
 
       t.column "user_id", :string, :limit => 50
+      t.column "title", :string, :limit => 20
       t.column "start_time", :time
       t.column "end_time", :time
-      t.column "amenities_id", :string
+      t.column "amenity_id", :string
       t.column "admin_id", :boolean
       t.column "weekday", :string
       t.column "date", :date
