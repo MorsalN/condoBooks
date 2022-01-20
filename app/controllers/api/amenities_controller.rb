faa01fbc3003935d2a775ca4a5954dfce5f6f10e
kunvar13 <kunvar13@gmail.com>
2022-01-20 10:49:27 -0600

    Worked on saving newly created amenities to database

--- app/controllers/api/amenities_controller.rb
+++ app/controllers/api/amenities_controller.rb
@@ -8,4 +8,15 @@ class Api::AmenitiesController < ApplicationController
   def show
   end
 
+  def create
+    amenity = Amenity.create!(:name => params["name"], :capacity => params["capacity"], :availability =>params[])
+
+    if (amenity)
+      render json: amenity
+    else
+      render json: amenity.errors
+    end
+
+  end
+
 end
