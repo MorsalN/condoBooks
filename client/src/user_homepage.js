4a6b2dc005f1aea0a0b21700846667e053194d8e
Morsal Niyaz <morsalniyaz@gmail.com>
2022-01-19 21:53:59 -0800

    Completed Homepage for Users

--- client/src/user_homepage.js
+++ client/src/user_homepage.js
@@ -1,27 +1,53 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import './css/User.css'
 
 
 export default function Homepage() { 
+
+  const navigate = useNavigate();
+
+  const params = useParams()
+  console.log(params)
+
+
+  function handleClick() {
+    navigate(`/${params.user_id}/booking`);
+  }
     return (
       <section className="User">
       <div className="User-box">
         <h1 className="User-title">Amenitiy Booking</h1>
 
-        <div className="how-book">
-          <h1>How to Book:</h1>
+        <table className="table">
+          <tbody id="customers-user">
 
-          <div>
-          <div class="numberCircle">1</div>
-          <div class="numberCircle">2</div>
-          <div class="numberCircle">3</div>
-          </div>
 
-          
+            <tr>
+              <td><h1>How To Book:</h1></td>
+              <td><div className="numberCircle">1</div></td>
+              <td><p className="home-text-1"><strong>Click </strong>on New Booking</p></td>
+            </tr>
 
-        </div>
+            <tr>
+              <td></td>
+              <td><div className="numberCircle">2</div></td>
+              <td><p className="home-text-1"><strong>Choose</strong> Amenity Room, Day and Time!</p></td>
+            </tr>
+
+            <tr>
+              <td></td>
+              <td><div className="numberCircle">3</div></td>
+              <td><p className="home-text-1"><strong>Confirm</strong> Booking</p></td>
+            </tr>
+
       
+           
+
+          </tbody>
+        </table>
+      
+              <button className="new" onClick={handleClick}>New Booking</button>
 
       </div>
     </section>
